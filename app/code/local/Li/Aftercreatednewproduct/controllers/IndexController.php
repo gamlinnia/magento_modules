27605fb7c4ac4321c76956bb8b37e4c819f12f95
gamlinnia <intppp01>
2015-10-31 08:36:36 +0800

    after created new product observer

--- app/code/local/Li/Aftercreatednewproduct/controllers/IndexController.php
+++ app/code/local/Li/Aftercreatednewproduct/controllers/IndexController.php
@@ -2,12 +2,6 @@
 
 class Li_Aftercreatednewproduct_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction () {
-        $this->loadLayout();
-        $parameters = array(
-            'product' => Mage::getModel('catalog/product')->load(1609),
-            'category' => Mage::getModel('catalog/category')->load(7)
-        );
-        Mage::dispatchEvent('aftercreatednewproduct_register_visit', $parameters);
-        $this->renderLayout();
+        Mage::dispatchEvent('aftercreatednewproduct_register_visit', null, 'customEvent.log');
     }
 }
