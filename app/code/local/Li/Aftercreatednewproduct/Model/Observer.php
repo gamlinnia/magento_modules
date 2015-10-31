5884065600883f2831bfdb24ba78c6f7f194f7f2
gamlinnia <intppp01>
2015-10-31 08:33:42 +0800

    after created new product observer

--- app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
+++ app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
@@ -10,4 +10,12 @@ class Li_Aftercreatednewproduct_Model_Observer {
         Mage::log($category->debug(), null, 'customEvent.log');
     }
 
+    public function savedProductCustomCallapi (Varien_Event_Observer $observer) {
+        Mage::log('run function savedProductCustomCallapi', null, 'customEvent.log');
+        /** @var $product Mage_Catalog_Model_Product */
+        $product = clone $observer->getEvent()->getProduct();
+        $originalStore = $product->getStoreId();
+        Mage::log($product->debug(), null, 'customEvent.log');
+    }
+
 }
